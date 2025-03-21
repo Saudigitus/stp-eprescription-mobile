d8cd69b09b7dca37416e023419e05e74d2c5abd9
CarlosMacaneta <carlosmacaneta95@gmail.com>
2025-03-21 08:30:31 +0200

    feat: adds app theme

--- app/src/main/java/org/saudigitus/e_prescription/presentation/EPrescriptionActivity.kt
+++ app/src/main/java/org/saudigitus/e_prescription/presentation/EPrescriptionActivity.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import org.saudigitus.e_prescription.presentation.theme.EprescriptionTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import org.saudigitus.e_prescription.presentation.theme.EPrescriptionTheme
 
 class EPrescriptionActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            EprescriptionTheme {
+            EPrescriptionTheme {
+                Scaffold { innerPadding ->
+                    Column(Modifier.fillMaxSize().padding(innerPadding)) {
 
+                    }
+                }
             }
         }
     }
