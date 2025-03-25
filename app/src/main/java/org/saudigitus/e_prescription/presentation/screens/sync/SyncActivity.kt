7e05d917c9f8fbc7689f7780181c892d6d720535
CarlosMacaneta <carlosmacaneta95@gmail.com>
2025-03-25 10:21:43 +0200

    feat: manifest dhis2 and work manager config and set edge to edge

--- app/src/main/java/org/saudigitus/e_prescription/presentation/screens/sync/SyncActivity.kt
+++ app/src/main/java/org/saudigitus/e_prescription/presentation/screens/sync/SyncActivity.kt
@@ -4,12 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import org.saudigitus.e_prescription.presentation.EPrescriptionActivity
 import org.saudigitus.e_prescription.presentation.theme.EPrescriptionTheme
@@ -22,7 +22,7 @@ class SyncActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge()
 
         setContent {
 
